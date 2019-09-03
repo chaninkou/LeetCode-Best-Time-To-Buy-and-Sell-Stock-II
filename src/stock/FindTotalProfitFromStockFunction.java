1de973d6e6fcad2ce6eb0a901c78b261a6855caa
chaninkou <michaelkoulove@yahoo.com>
2019-09-03 00:42:09 -0700

    Ok easy problem. Able to get the total of profit of stock.

---
+++ src/stock/FindTotalProfitFromStockFunction.java
@@ -0,0 +1,17 @@
+package stock;
+
+public class FindTotalProfitFromStockFunction {
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        
+        for(int i = 0; i < prices.length - 1; i++){
+            // If next element is greater than current element
+            if(prices[i + 1] > prices[i]){
+                // Add the profit to the total
+                total += prices[i + 1] - prices[i];
+            }
+        }
+            
+        return total;
+    }
+}
