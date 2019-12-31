4fb9ce6ef3a01ea2a85077591cb6d6ee0ecaf211
chaninkou <michaelkoulove@yahoo.com>
2019-12-30 16:07:40 -0800

    Great review on finding pattern to only add profit to total.

--- src/stock/FindTotalProfitFromStockFunction.java
+++ src/stock/FindTotalProfitFromStockFunction.java
@@ -4,11 +4,12 @@ public class FindTotalProfitFromStockFunction {
     public int maxProfit(int[] prices) {
         int total = 0;
         
-        // also o(n)
+        // also o(n), going through the whole array, prices.length - 1 because i + 1
         for(int i = 0; i < prices.length - 1; i++){
             // If next element is greater than current element
-            if(prices[i + 1] > prices[i]){
-                // Add the profit to the total
+        	// Since we only want to add the number that is greater than 0
+            if(prices[i + 1] > prices[i]){  
+                // Only add the profit to the total
                 total += prices[i + 1] - prices[i];
             }
         }
